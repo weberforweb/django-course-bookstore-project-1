246b0982205b179369716c2d46cc11b233e0a162
Weber for Web <weberforweb@gmail.com>
2022-11-12 17:11:21 -0500

    create authictication pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+
+from .forms import CustomUserCreationForm
+
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "registration/signups.html"
+    success_url = reverse_lazy('login')
 
 # Create your views here.
